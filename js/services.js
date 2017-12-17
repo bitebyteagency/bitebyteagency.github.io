e5358f44c4ff321052e4d4e7e320f898f75732b1
bitebyteagency <34159469+bitebyteagency@users.noreply.github.com>
2017-12-17 20:30:35 +0200

    Update services.js

--- js/services.js
+++ js/services.js
@@ -13,7 +13,18 @@ if($('.navbar').length > 0){
         checkScroll();
     });
 }
-
+document.onreadystatechange = function () {
+  var state = document.readyState
+  if (state == 'interactive') {
+       document.getElementById('contents').style.visibility="hidden";
+  } else if (state == 'complete') {
+      setTimeout(function(){
+         document.getElementById('interactive');
+         document.getElementById('load').style.visibility="hidden";
+         document.getElementById('contents').style.visibility="visible";
+      },3000);
+  }
+}
 
 var isSelected = false;
 const myProjects = [
